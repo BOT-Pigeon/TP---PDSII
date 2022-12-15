230b0a0778185b2ec07fd9de681173cb0e71add0
Felipe Meireles Leonel <69362448+felipemleonel@users.noreply.github.com>
2022-12-15 17:09:47 -0300

    Update Arena.h

--- include/Arena.h
+++ include/Arena.h
@@ -6,12 +6,13 @@
 
 class Arena{
     private:
-        vector<Player> players;
+        Player Player1;
+        Player Player2;
     public:
-        Arena(Player player1, Player player2);
-        int balanceamento_por_elemento();
-        bool ganhou();
-        string ataque(Pokemon agressor, Pokemon atacado, int danificado);
+        Arena(Player* player1, Player* player2);
+        int balanceamento_por_elemento(const char* elemento1,const char* elemento2);
+        bool ganhou(int vida1,int vida2);
+        string ataque(Pokemon atacado, int danificado);
         void game();
 };
 
