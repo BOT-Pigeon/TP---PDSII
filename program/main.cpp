0951f3ab353e19c98c1b5976c336356845b3e2e6
MaxPe10 <95247715+MaxPe10@users.noreply.github.com>
2022-12-22 03:33:30 -0300

    funcao jogar denovo

--- program/main.cpp
+++ program/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <string>
+#include <cstring>
 
 #include "player.h"
 #include "pokemon.h"
@@ -15,6 +15,7 @@ Player cria_player(string nome, Pokemon pokemon){
 
 int main() { 
 
+    int Jogarnovamente;
     
     cout << "-----------------------------------" << endl;
     cout << "           INICIO DO JOGO          " << endl;
@@ -22,7 +23,7 @@ int main() {
     cout << endl;
     cout << endl;
 
-    Arena batalha = Arena();    
+    REFAZ:Arena batalha = Arena();    
     batalha.players_escolhem_pokemon();
     // Após os Player escolherem seus Pokemons, o jogo se inicia
     cout << endl;
@@ -32,5 +33,16 @@ int main() {
     batalha.game();
 
     cout << "FIM DO JOGO" << endl;
+    
+    cout << "Caso desejar, digite 1 para jogar novamente" << endl;
+
+    cin >> Jogarnovamente;
+    
+    if(Jogarnovamente==1){
+        
+        goto REFAZ;
+        
+    }
+    
     return 0;
 }
